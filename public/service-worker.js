2a605a6fc06ee6921e1eb0c544d430b8f8011853
Mickey Phillips <phillipsmickey41@gmail.com>
2021-12-28 22:03:25 -0600

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const DATA_CACHE_NAME = 'data-cache-01';
 const FILES_TO_CACHE = [
     "/",
     "./index.html",
-    "./css/style.css",
+    "./css/styles.css",
     "./js/idb.js",
     "./js/index.js",
     "./manifest.json"
